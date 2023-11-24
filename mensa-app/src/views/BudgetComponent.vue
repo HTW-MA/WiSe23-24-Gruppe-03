f000631aee7433fc64315e1790184bb05db01111
Miroslaw-Keil <Miroslaw.Keil@Student.HTW-Berlin.de>
2023-11-24 19:21:49 +0100

    versuch das Modal automatisch zu schließen

--- mensa-app/src/views/BudgetComponent.vue
+++ mensa-app/src/views/BudgetComponent.vue
@@ -1,11 +1,16 @@
-<script lang="ts">
+<script lang="js">
+import PopUp from "@/components/PopUp.vue";
+
 
 export default {
   name: 'BudgetComponent',
+  components: {PopUp},
   methods: {
     closeModal() {
-      //setTimeout(function() {$('#exampleModal').modal('hide')}, 5000)
-      // Hier irgendwie das Modal nach x Sekunden schließen
+      setTimeout(() => {
+        this.$refs.myModal.close(); // Assuming you have a method named close() in your modal component
+      }, 4000);
+      //const modal = document.getElementById("popupId")
     }
   }
 }
@@ -14,24 +19,9 @@ export default {
 
 <template>
   <!-- Click on img triggers modal -->
-  <img src="../assets/NFC_Placeholder.png" alt="NFC" data-bs-toggle="modal" data-bs-target="#exampleModal" style="cursor: pointer" v-on:click="closeModal()">
-
+  <img v-on:click="closeModal" src="../assets/NFC_Placeholder.png" alt="NFC" data-bs-toggle="modal" data-bs-target="#exampleModal" style="cursor: pointer">
   <!-- Modal -->
-  <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-    <div class="modal-dialog modal-dialog-centered">
-      <div class="modal-content">
-        <div class="modal-header">
-          <h1 class="modal-title fs-5" id="exampleModalLabel">Bitte scannen Sie ihre Karte</h1>
-          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-        </div>
-        <div class="modal-body">
-          <div class="spinner-border text-primary" role="status">
-            <span class="visually-hidden">Loading...</span>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
+  <PopUp ref="test"/>
 </template>
 
 <style scoped>
