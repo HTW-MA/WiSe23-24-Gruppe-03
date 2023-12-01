3b59bbdcf783c310b713b9c52022bd29eb81ea4d
Miroslaw-Keil <Miroslaw.Keil@Student.HTW-Berlin.de>
2023-12-01 21:05:51 +0100

    Automatischer Service Worker funktioniert

--- mensa-app/src/main.ts
+++ mensa-app/src/main.ts
@@ -1,6 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import "/register-service-worker"
+import "./registerServiceWorker"
 import router from "../router";
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.min'
@@ -15,7 +15,7 @@ createApp(App).use(router).provide('store', store).mount('#app')
 
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
-        navigator.serviceWorker.register('/register-service-worker.js')
+        navigator.serviceWorker.register('/service-worker.js')
             .then(registration => {
                 console.log('Service Worker registered with scope:', registration.scope);
             })
--- mensa-app/vue.config.js
+++ mensa-app/vue.config.js
@@ -2,3 +2,21 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true
 })
+
+// module.exports = {
+//   pwa: {
+//     name: 'My App',
+//     themeColor: '#4DBA87',
+//     msTileColor: '#000000',
+//     appleMobileWebAppCapable: 'yes',
+//     appleMobileWebAppStatusBarStyle: 'black',
+//
+//     // configure the workbox plugin
+//     workboxPluginMode: 'InjectManifest',
+//     workboxOptions: {
+//       // swSrc is required in InjectManifest mode.
+//       swSrc: './service-worker.js',
+//       // ...other Workbox options...
+//     }
+//   }
+// }
