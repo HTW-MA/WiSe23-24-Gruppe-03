634903fc286fb24b5b0ef4421d7b7f48a9ed6ea0
Anton Pohle <s0582388@htw-berlin.de>
2023-12-09 14:40:23 +0100

    psuh me

--- mensa-app/src/components/MenuDisplay.vue
+++ mensa-app/src/components/MenuDisplay.vue
@@ -8,7 +8,7 @@
 
   <div v-else>
     <div v-if="isWeekend">
-      Am Wochenende hat die Mensa zu - Geh nach Hause, kleiner Streber!
+      Am Wochenende hat die Mensa zu -  Geh nach Hause, kleiner Streber!
     </div>
 
     <div v-else-if="Object.keys(meals).length === 0 && !isWeekend">
